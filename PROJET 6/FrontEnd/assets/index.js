50d9e12210a8c721eaecd79767144536f563d9d4
Shameyah <dmaiga.traore@gmail.com>
2024-05-28 05:08:53 +0200

    affiche des travaux ok

--- PROJET 6/FrontEnd/assets/index.js
+++ PROJET 6/FrontEnd/assets/index.js
@@ -3,12 +3,14 @@ let works = []; // Pour stocker les projets
 
 function displayWorks(works) {
     works.forEach(work => {
+        console.log('Affichage du projet:', work); // Vérification des données du projet
+
         const figure = document.createElement('figure');
         const img = document.createElement('img');
         const figcaption = document.createElement('figcaption');
 
-        img.src = work.imageURL;
-        img.alt = work.title;
+        img.src = work.imageUrl;
+        img.alt = work.title;    
         figcaption.textContent = work.title;
 
         figure.appendChild(img);
@@ -25,6 +27,7 @@ fetch ('http://localhost:5678/api/works/')
     .then(response => response.json())
     .then(data => {
         works = data; // stocker les projets
+        console.log('Données des travaux récupérées:', works); // Vérification des données récupérées
         displayWorks(works); // Afficher les projets
     })
     .catch(error => console.error('Erreur lors de la recuperation:', error));
