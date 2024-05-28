43bc34f3d82b0c89fab47b389dc9debbadd3018c
Shameyah <dmaiga.traore@gmail.com>
2024-05-28 05:37:09 +0200

    recuperation des categories + ajout des filtres

--- PROJET 6/FrontEnd/assets/index.js
+++ PROJET 6/FrontEnd/assets/index.js
@@ -1,6 +1,9 @@
 const gallery = document.querySelector('.gallery');
+const filtersContainer = document.querySelector('#all-filters');
 let works = []; // Pour stocker les projets
+let categories = []; // Pour stocker les catégories
 
+// Fonction pour afficher les projet en ajouter un element Figure à la galerie
 function displayWorks(works) {
     works.forEach(work => {
         console.log('Affichage du projet:', work); // Vérification des données du projet
@@ -22,6 +25,32 @@ function displayWorks(works) {
     
 }
 
+// Fonction pour afficher les projets filtrés
+function  displayFilteresWorks(categoryId) {
+    gallery.innerHTML = ''; //Nettoyer la galerie
+    works.filter(work => categoryId === 'all' || work.categoryId === categoryId)
+        .forEach(work => displayWorks(works));
+
+}
+
+//Fonction pour générer les filtres de catégories
+function generateCategoryFilters() {
+    const allFilter = document.createElement('button');
+    allFilter.textContent = 'Tous';
+    allFilter.classList.add('filters-design', 'filter-all');
+    allFilter.addEventListener('click', () => displayFilteresWorks('all'));
+    filtersContainer.appendChild(allFilter);
+
+    categories.forEach(category => {
+        const filter = document.createElement('button');
+        filter.textContent = category.name;
+        filter.classList.add('filters-design');
+        filter.addEventListener('click', () => displayFilteresWorks(category.id));
+        filtersContainer.appendChild(filter);
+    
+    });
+}
+
 // Récupérer les données des travaux via l'API
 fetch ('http://localhost:5678/api/works/')
     .then(response => response.json())
@@ -31,3 +60,12 @@ fetch ('http://localhost:5678/api/works/')
         displayWorks(works); // Afficher les projets
     })
     .catch(error => console.error('Erreur lors de la recuperation:', error));
+
+// Récupérer les categories via l'API
+fetch('http://localhost:5678/api/categories')
+      .then(response => response.json())
+      .then(data => {
+          categories = data; // Stocker les catégories
+          generateCategoryFilters(); // Générer les filtres
+      })
+      .catch(error => console.error('Erreur lors de la récupération des catégories :', error));
